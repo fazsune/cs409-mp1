a137943375335d5d70b8906f298fbf2cdd3999e5
Fazsune <126736266+fazsune@users.noreply.github.com>
2026-09-22 23:37:09 -0500

    MP1 Website

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,17 @@
 /* Your JS here. */
-console.log('Hello World!')
+// console.log('Hello World!')
+
+const navbar = document.getElementById("navbar");
+window.addEventListener("scroll", () => {
+	if (window.scrollY > 60) {
+		navbar.style.height = "40px";
+		navbar.querySelectorAll("button").forEach(button => {
+			//button.style["font-size"] = "40cqh";
+		});
+	} else {
+		navbar.style.height = "60px";
+		navbar.querySelectorAll("button").forEach(button => {
+			//button.style["font-size"] = "30cqh";
+		});
+	}
+});
